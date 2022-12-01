def5ecb2571eb150a4572f034694d1fa7d593ad3
olexander.hritsun <olexander.hritsun@CluePoints.com>
2022-12-01 23:37:37 +0200

    added tests for posts api

--- config/devConfig.ts
+++ config/devConfig.ts
@@ -2,7 +2,7 @@ import { basicConfig } from "./basicConfig";
 const cypress = require("cypress");
 
 const devConfig = Object.assign(basicConfig);
-devConfig.e2e.baseUrl = "https://jsonplaceholder.typicode.com/";
+devConfig.e2e.baseUrl = "https://jsonplaceholder.typicode.com";
 
 cypress.open({
     config: {
